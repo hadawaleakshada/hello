233b09a6a71bf2b2b7c201fa3a4f20c245e1b7dc
akshada.hadawale <akshadahadawale12@gmail.com>
2025-02-20 14:55:13 +0530

    EvenOdd program

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import { useState } from "react";
 
-function App(){
-    let[count,setCount]=useState(1);
-    function checkTheCount(){
-        setCount(count++)
-        
-    
+function App() {
+    let [number, setNumber] = useState("");
+
+    function checkTheEvenOdd() {
+        if (number % 2 == 0) {
+        setNumber(number+"is even");
+
+        }
+        else{
+            setNumber(number+"is odd");
+            
+        }
+
+
     }
 
 
-return(
-    <div>
-        <div>{count}</div>
-        <button onClick={checkTheCount}>check</button>
+    return (
+        <div>
+            <h1>{number}</h1>
+            <input type="text" onChange={(e)=>{setNumber(e.target.value)}}/>
+            <button onClick={checkTheEvenOdd}>check</button>
         </div>
 
-);
+    );
 }
 export default App;
 
