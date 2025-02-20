006b50254d2d545a569c2831484bcd24e8ea730b
akshada.hadawale <akshadahadawale12@gmail.com>
2025-02-20 16:01:15 +0530

    evenOddsecond program

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import { useState } from "react";
 
 function App() {
-    let [number, setNumber] = useState("");
-
-    function checkTheEvenOdd() {
-        if (number % 2 == 0) {
-        setNumber(number+"is even");
-
-        }
-        else{
-            setNumber(number+"is odd");
-            
-        }
-
-
+    
+    let [number, setNumber] = useState(" ")
+    function setInput(event){
+    setNumber(event.target.value);
     }
+        function checkTheEvenOdd() {
+
+            if (number % 2 == 0) {
+                setNumber(number + "is even");
+
+            }
+            else {
+                setNumber(number + "is odd");
+
+            }
+
+
+        }
+    
+    
 
 
     return (
         <div>
             <h1>{number}</h1>
-            <input type="text" onChange={(e)=>{setNumber(e.target.value)}}/>
+            <input type="text" onChange={setInput} />
             <button onClick={checkTheEvenOdd}>check</button>
         </div>
 
     );
 }
+
 export default App;
 
 
